99d4be507428cbe6d155116f9daea1ce3c7909a9
vincicord <103143956+vincicord@users.noreply.github.com>
2022-04-14 17:54:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import threading
 
 import discord
@@ -19,15 +20,35 @@ except ImportError:
 
 token = os.environ["token"]
 bot = commands.Bot(command_prefix='1dankmemereventfarmer1',self_bot=True)
-disbot = discum.Client(token=token, log=False)
+disbot = discum.Client(token=token, log={"console": False,"file":"log.txt"})
 bot.remove_command("help")
+
+extensions = ["events"]
 
 ownerid = "902195286544384070"
 dankmemerid = "270904126974590976"
 
+def restart_bot(): 
+  os.execv(sys.executable, ['python'] + sys.argv)
+
+def setupcogs():
+  print("\nLoading cogs...")
+  for extension in extensions:
+    try:
+      bot.load_extension(f"cogs.{extension}")
+      print(f"Loaded [ {extension} ]")
+    except Exception as error:
+      print(f"Error loading [ {extension} ] // [ {error} ]")
+      restart_bot()
+
+
+
+
+
 
 def gtway():
-  disbot.gateway.run(auto_reconnect=True)
+  disbot.gateway.run()
+  disbot.gateway.log = {"console":False, "file":"log.txt"}
 
 def gtwaythread():
   x = threading.Thread(target=gtway)
@@ -45,7 +66,8 @@ def tapbuttonnolink(strguildid,strchannelid,strmsgid,choice):
   disbot.click(data["author"]["id"],channelID=data["channel_id"],guildID=strguildid,messageID=data["id"],messageFlags=data["flags"],data=buts.getButton(choice))
 
 
+
 if __name__ == "__main__":
-  bot.load_extension("cogs.events")
-  bot.run(token,bot=False)
+  setupcogs()
   gtwaythread()
+  bot.run(token,bot=False)
